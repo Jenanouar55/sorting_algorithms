3e983d150932d7feba29caba2d05e877decf6640
jenanouar1234 <jenanouar1234@gmail.com>
2024-04-27 10:48:24 +0100

    pushed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,13 +9,11 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int x, y;
 	int tmp;
-	int swapp;
 
 	if (size <= 1)
 		return;
 		for (x = 0; x < size - 1; x++)
 		{
-			swapp = 0;
 			for (y = 0; y < size - 1 - x; y++)
 			{
 				if (array[y] > array[y + 1])
@@ -23,12 +21,8 @@ void bubble_sort(int *array, size_t size)
 					tmp = array[y];
 					array[y] = array[y + 1];
 					array[y + 1] = tmp;
-					swapp = 1;
 					print_array(array, size);
-					}
+				}
 			}
-					if (swapp == 0)
-					break;
-	}
+		}
 }
-
